e65b2eaa2b85f8cd8176ebca5e0be27b7a50ffd0
Alibek Allamzharov <alibek.b1@icloud.com>
2023-04-23 19:10:41 +0600

    Create MinimumSumofFourDigitNumberAfterSplittingDigits.java

---
+++ Leetcode/MinimumSumofFourDigitNumberAfterSplittingDigits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumSum(int num) {
+        int[] arr = new int[4];
+        int cur = 0;
+
+        while (num > 0) {
+            arr[cur++] = num % 10;
+            num /= 10;
+        }
+
+        Arrays.sort(arr);
+        return (arr[0] * 10 + arr[2]) + (arr[1] * 10 + arr[3]);
+    }
+}
